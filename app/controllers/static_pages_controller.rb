da44ab1cbc7a2758a1a77c5bcf7118b5f095bfcb
roxwelruiz <roxwel.ruiz@gmail.com>
2017-02-23 16:20:19 -0500

    4.3 Create orders controller, actions, and views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def landing_page
+  	# retrieve the first product in Products table and set it to @featured_product
+  	@featured_product = Product.first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   resources :products
-  root 'static_pages#index'
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  #root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   get '/about', to: 'static_pages#about'
 	get '/contact', to: 'static_pages#contact'
