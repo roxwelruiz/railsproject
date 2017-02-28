578ee8fc49ec473427a781e5771d1455ca14aa67
roxwelruiz <roxwel.ruiz@gmail.com>
2017-02-28 17:28:51 -0500

    4.5 Add product carousel and clean code with partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
   def landing_page
   	# retrieve the first product in Products table and set it to @featured_product
   	@featured_product = Product.first
+  	@products = Product.limit(3)
   end
 
 end
