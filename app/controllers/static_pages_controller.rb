31727d518eaaa940e1ddac62c07225d07124fbe6
roxwelruiz <roxwel.ruiz@gmail.com>
2017-03-07 20:53:44 -0500

    5.1 Update site root and Added routes to point back to home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,14 @@
 class StaticPagesController < ApplicationController
-  def index
+  def preindex
   end
 
-  def landing_page
-  	# retrieve the first product in Products table and set it to @featured_product
+  def go_to_main
+  	redirect_to "/index"
+  end
+
+  def index
+   	# retrieve the first product in Products table and set it to @featured_product
   	@featured_product = Product.first
   	@products = Product.limit(3)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
 
   #root 'static_pages#index'
-  root 'static_pages#landing_page'
+  root 'static_pages#preindex'
 
+  get '/main', to: 'static_pages#go_to_main'
+  get '/index', to: 'static_pages#index'
   get '/about', to: 'static_pages#about'
 	get '/contact', to: 'static_pages#contact'
 
