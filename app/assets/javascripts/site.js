f17cf4efc4699e2e6ddf5c34ce64c0b8bb63e15c
roxwelruiz <roxwel.ruiz@gmail.com>
2017-04-04 20:46:32 -0400

    5.11 Revised Submission

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -14,4 +14,32 @@ $(document).on('turbolinks:load', function() {
   		return $(this).attr('data-score');
   	}
   });
+
+  $('#contact-button').on('click', function() {
+    $('#error_message').html('');
+    $('#message').css('border', '');
+    $('#email').css('border', '');
+
+    var err_count = 0;
+    var inputEmail = $('#email').val();
+    var inputMsg = $('#message').val();
+
+    if(inputEmail === '' || inputMsg === '') {
+      if(inputMsg === '') {
+        $('#message').css('border', '3px solid #c00');
+        $('#message').focus();
+        err_count += 1;
+      }
+      if(inputEmail === '') {
+        $('#email').css('border', '3px solid #c00');
+        $('#email').focus();
+        err_count += 1;
+      }
+
+      $('#error_message').html("<div id='error_explanation'><h2>" + err_count + " value(s) missing.</h2></div>");
+      return false;
+    }
+    else
+      return true;
+  });
 });
