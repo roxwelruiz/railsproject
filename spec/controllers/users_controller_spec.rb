ee5a23177ab44d4a26351d0b285d4a981874599d
roxwelruiz <roxwel.ruiz@gmail.com>
2017-04-06 18:16:51 -0400

    6.3 Revised submission

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,37 +1,38 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-
-	let(:user) { User.create!(email: 'test@test.com', password: 'testing') }
-	let(:user_b) { User.create!(email: 'admin@test.com', password: 'admin1') }
+	before do
+		@user1 = User.create!(email: 'test@test.com', password: 'testing')
+		@user2 = User.create!(email: 'admin@test.com', password: 'admin1')
+	end
 
 	describe 'GET #show' do
+
 		context 'User is logged in' do
 			before do
-				sign_in user
+				sign_in @user1
 			end
 
 			it 'loads correct user details' do
-				get :show, params: { id: user.id }
+				get :show, params: { id: @user1.id }
 				expect(response).to have_http_status(200)
-				expect(assigns(:user)).to eq user
+				expect(assigns(:user)).to eq @user1
+			end
+
+			it 'cannot access show page of user2' do
+				get :show, params: { id: @user2.id }
+				expect(response).to have_http_status(302)
+				expect(assigns(:user)).not_to eq @user1
+				expect(response).to redirect_to(root_path)
 			end
 		end
 
 		context 'no user is logged in' do
 			it 'redirects to sign in' do
-				get :show, params: { id: user.id }
-				expect(response).to redirect_to(new_user_session_path)
+				get :show, params: { id: @user1.id }
+				expect(response).to redirect_to(root_path)
 			end
 		end
 
-		context 'first user cannot access second user' do
-			it 'redirects to home' do
-				get :show, params: { id: user.id }
-				expect(response).to have_http_status(302)
-				expect(assigns(:user_b)).not_to eq user
-				expect(response).to redirect_to(new_user_session_path)
-			end
-		end
 	end
 end
